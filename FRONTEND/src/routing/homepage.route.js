ea934d6a5bfc2a70c59d0371af0ea26bffb5b2a5
Sohaibkundi2 <sohaibkundi2@gmail.com>
2025-06-11 13:25:59 +0500

    fix: Correct import path for routeTree in homepage routing

--- FRONTEND/src/routing/homepage.route.js
+++ FRONTEND/src/routing/homepage.route.js
@@ -1,7 +1,7 @@
 
 import { createRoute } from '@tanstack/react-router'
 import HomePage from '../pages/HomePage'
-import { rootRoute } from './RouteTree'
+import { rootRoute } from './routeTree.js'
 
 export const HomePageRoute = createRoute({
   getParentRoute: () => rootRoute,
