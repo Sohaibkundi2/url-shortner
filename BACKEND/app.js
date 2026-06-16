9524b2e48ed0bb54cf7a0f9822bca3f10b3a515b
Sohaibkundi2 <sohaibkundi2@gmail.com>
2026-06-16 21:18:04 +0500

    fix: update CORS origin to use new URL for production

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express()
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://shrtit.tech', 'https://www.shrtit.tech','https://url-shortner-production-2288.up.railway.app'],
+    origin: ['http://localhost:5173', 'https://shrtit.tech', 'https://www.shrtit.tech','https://urlchanged.vercel.app'],
     credentials: true
 }));
 
